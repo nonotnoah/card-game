7140d3eba10625eb0c05e44ff4f764492cb5a384
Noah Myers <alphazazoo@gmail.com>
2022-12-11 02:00:42 -0800

    clientsite socket fiddling

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,32 +1,48 @@
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState, useRef, MutableRefObject } from 'react'
 import '../styles/App.css'
 import { io, Socket } from 'socket.io-client'
+import Card from './Cards'
+
+interface drawPayload {
+    card1: string[],
+    card2: string[],
+    match: string
+}
 
 function App() {
-  // connect client
-  // const connect = () => {
-  //   const socket = io('http://localhost:5000')
-  // }
+    const [showCard, setShowCard] = useState<Boolean>(false)
+    // connect client
+    // const socket: MutableRefObject<Socket>['current'] = useRef(io('http://localhost:5000'))
+    const socket: MutableRefObject<Socket> = useRef(io('http://localhost:5000'))
+    // const socket: any = useRef(io('http://localhost:5000'))
 
-  // useEffect(() => {
-  //   connect()
-  // }, [])
-  const socket = io('http://localhost:5000')
+    let card1
+    let card2
+    let match
 
-  socket.on('draw', (val) => {
-    const card1 = val.card1
-    const card2 = val.card2
-    const match = val.match
-    console.log(val)
-  })
+    socket.current.on('draw', (val: drawPayload) => {
+        card1 = val.card1
+        card2 = val.card2
+        match = val.match
+        setShowCard(true)
+        console.log(val)
+        console.log(showCard)
+    })
 
-  // value can be accessed with playerCount.current
-  // does not trigger a re-render
-  const playerCount = useRef(0)
-
-  return (
-    <h1>Test</h1>
-  );
-
+    return (
+        <div className="wrapper">
+            {showCard ? (
+                <>
+                    <Card card={card1} />
+                    <Card card={card2} />
+                    <div className="match">{match}</div>
+                </>
+            ) : (
+                <>
+                    <h1>Nothing here!</h1>
+                </>
+            )}
+        </div>
+    );
 }
 export default App
